aa27357e7c2b11e6bd1fe260653c360bdaa1d3c8
Asher Heywood Peters <339513541@GoTVDSB.ca>
2023-09-27 10:26:13 -0400

    Done.

--- Part 4 random numbers/Program.cs
+++ Part 4 random numbers/Program.cs
@@ -11,23 +11,17 @@ namespace Part_4_random_numbers
         static void Main(string[] args)
         {
             Random generator = new Random();
-            int num1, num2, num3, num4, num5;
-            string ha = "HA";
-            num1 = generator.Next(1, 21);
-            num2 = generator.Next(21, 41);
-            num3 = generator.Next(41, 61);
-            num4 = generator.Next(61, 81);
-            num5 = generator.Next(81, 101);
-            for (int ctr = 0; ctr < 10; ctr++)
-            {
-                Console.Write("{0,3}   ", generator.Next(num1, num2));
-            }
-            for (int ctr = 0; ctr < num5; ctr++)
-            {
-                Console.Write("{0,-19:R}   ", generator.NextDouble());
-                if ((ctr + 1) % 3 == 0) Console.WriteLine();
-            }
-            
+            int min, max;
+            min = generator.Next(0, (50 +- 1));
+            max = generator.Next(0, (50 +- 1));
+            Console.WriteLine(generator.NextDouble());
+            Console.WriteLine(generator.Next());
+            Console.WriteLine(generator.Next(0, 11));
+            Console.WriteLine(generator.Next(10, 21));
+            Console.WriteLine(generator.Next(min, max));
+            //If min > max it creates an error.
+            Console.WriteLine(generator.Next(-10, -5));
+            //You can include negative numbers.
         }
     }
 }
